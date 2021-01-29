59f2ee003a7e6dbdfa0956bb1b56dbb3f68c5a0f
Maybiz <maxime.julliant@laposte.net>
2021-01-29 15:48:19 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Formik } from 'formik'
+import * as Yup from 'yup'
 
 export class App extends Component {
+
+  userSchema = Yup.object().shape({
+    name: Yup.string().min(3, 'Trop court !').max(20, 'Trop long !').required('Requis'),
+    email: Yup.string().email("L'email doit être valide"),
+    password: Yup.string().min(5, 'Trop court')
+  })
 
   submit = (values, actions) => {
     console.log(values)
@@ -11,19 +18,10 @@ export class App extends Component {
     }, 1000);
   }
 
-  validate = values => {
-    let errors = {}
-    if(values.name && values.name.length < 3) {
-      errors.name = 'Nom trop court'
-    }
-    return errors
-  }
-
-
   render() {
     return (
 			<div className="container-fluid p-5 bg-light d-flex flex-column justify-content-center align-items-center">
-				<Formik onSubmit={this.submit} initialValues={{ name: '', email: '', password: '' }} validate={this.validate}>
+				<Formik onSubmit={this.submit} initialValues={{ name: '', email: '', password: '' }} validationSchema={this.userSchema} >
           
 					{({ values, handleBlur, handleChange, handleSubmit, isSubmitting, errors, touched }) => (
 
@@ -39,11 +37,15 @@ export class App extends Component {
 							<div className="form-group">
 								<label>Adresse email</label>
 								<input type="email" name="email" className="form-control" onChange={handleChange} onBlur={handleBlur} value={values.email} />
+                {errors.email && touched.email 
+                && <div className="text-danger">{errors.email}</div>}
 							</div>
 
 							<div className="form-group">
 								<label>Mot de passe</label>
 								<input type="password" name="password" className="form-control" onChange={handleChange} onBlur={handleBlur} value={values.password} />
+                {errors.password && touched.password 
+                && <div className="text-danger">{errors.password}</div>}
 							</div>
 
 							<button type="submit" className="btn btn-primary" disabled={isSubmitting}>Envoyer</button>
